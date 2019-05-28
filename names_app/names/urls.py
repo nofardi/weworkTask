317784980271904a582dc11d754a6c0c95fa57f8
Nofar Diamant <nofardiamant@Nofars-MacBook-Pro.local>
2019-05-28 12:55:09 +0300

    task2

--- names_app/names/urls.py
+++ names_app/names/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from names import views
-
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    url(r'', views.HomePageView.as_view()),
+    url(r'', csrf_exempt(views.HomePageView.as_view())),
 ]
--- names_app/names/views.py
+++ names_app/names/views.py
@@ -5,10 +5,15 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 
-
+names2 = ['Jhon', 'Elton', 'Michael']
 names = "Jhon, Elton, Micheal"
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        html = "<html><body><div><p1>Names:</p1>" + names + "</br></br></br></div><form method=\"post\" action=\"http://localhost:8000\"> Name:<input type=\"text\" name=\"name\" /><input type=\"submit\" value=\"Submit\" /></form></body></html>"
+        html = "<html><body><div><p1>Names:</p1>" + ','.join(names2) + "</br></br></br></div><form method=\"post\" action=\"http://localhost:8000\"> Name:<input type=\"text\" name=\"name\" /><input type=\"submit\" value=\"Submit\" /></form></body></html>"
         return HttpResponse(html);
+
+    def post(self, request, **kwargs):
+        name = request.POST.get("name");
+        names2.append(name);
+        return HttpResponse(200);
